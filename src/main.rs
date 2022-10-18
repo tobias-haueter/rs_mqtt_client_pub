5ba4e4b9f79d21c43a6b12f9dfbafe4dd039dfda
Tobias Haueter <82332325+tobias-haueter@users.noreply.github.com>
2022-10-18 19:53:54 +0200

    Update for clean CLI input with options
    With multi file script. (main.rs, input.rs, client.rs)

--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,30 @@
-use std::process;
-use std::{thread, time, io};
-use chrono::{Local};
 use figlet_rs::FIGfont;
+use colored::*;
 
-extern crate paho_mqtt as mqtt;
+mod client;
+mod input;
 
 fn main() {
-    // CLI style ----------------------------------------------------------------------------------
 
-    // create header
+    // CLI Style
     let standard_font = FIGfont::standard().unwrap();
-    let title = standard_font.convert("mqtt_client");
-    assert!(title.is_some());  
+    let figure = standard_font.convert("rs_MQTT");
+    assert!(figure.is_some());
 
-    println!("--------------------------------------------------------------------------------");
-    println!("RUST");
-    println!("{}", title.unwrap());
-    println!("Local Time Publisher");
-    println!("--------------------------------------------------------------------------------");
+    println!("----------------------------------------------------------------");
+    println!("{}", figure.unwrap());
+    println!("{}","Welcome to RUST MQTT Publisher Client!".blue());
+    println!("{}"," - Publishing [1Hz] with: index | local_time_now".blue());
+    println!("{}"," - Topic: DateTimeLocal/".blue());
+    println!("----------------------------------------------------------------");
 
-    // Connection data user input -----------------------------------------------------------------
-    let mut ip_broker = String::new();
-    let mut port_broker = String::new();
-
-
-    println!("Chooes Broker IP: ");
-    io::stdin()
-    .read_line(&mut ip_broker)
-    .expect("Failed to read ip address");
-    println!("IP_BROKER -> {}", ip_broker.to_string());
-    println!("--------------------------------------------------------------------------------");
-
-    println!("Chooes Broker PORT: ");
-    io::stdin()
-    .read_line(&mut port_broker)
-    .expect("Failed to read port number");
-    println!("PORT_BROKER -> {}", port_broker.to_string());
-    println!("--------------------------------------------------------------------------------");
-
-    let connection_string = format!("tcp://{}:{}", ip_broker.trim(), port_broker);
-    println!("-- Connection String -> {}", connection_string.trim());
-    println!("-- Topic -> [DateTimeLocal]");
-    println!("-- End Publishing -> [CTRL + C]");
-    println!("--------------------------------------------------------------------------------");
-
-
-    // Create a client & define connect options "tcp://localhost:1883" ----------------------------
-    let cli = mqtt::Client::new(connection_string).unwrap_or_else(|err| {
-        println!("Error creating the client: {:?}", err);
-        process::exit(1);
-    });
-
-    let conn_opts = mqtt::ConnectOptionsBuilder::new()
-        .keep_alive_interval(time::Duration::from_secs(20))
-        .clean_session(true)
-        .finalize();
-
-    // Connect and wait for it to complete or fail
-    if let Err(e) = cli.connect(conn_opts) {
-        println!("Unable to connect:\n\t{:?}", e);
-        process::exit(1);
-    }
-
-    // Create a message and publish loop ----------------------------------------------------------
-    let delay = time::Duration::from_millis(1000);
-    let mut index = 0;
-    loop {
-        let local_time = Local::now();
-
-        // creating message
-        index = index + 1;
-        let msg = format!("{} | local_time_now_is: {}", index.to_string(), local_time);
-        println!("{}",msg);
-        // payload
-        let msg = mqtt::Message::new("DateTimeLocal", msg, mqtt::QOS_0);
-        let tok = cli.publish(msg);
-        
-
-        // sending error
-        if let Err(e) = tok {
-        println!("Error sending message: {:?}", e);
-
-        // Disconnect from the broker
-        let tok = cli.disconnect(None);
-        // tok.wait().unwrap();
-        tok.unwrap();
-        }
-
-        thread::sleep(delay);
+    if input::msg_start() {
+        client::publisher(
+            input::host_ip_v4(), 
+            input::host_port(), 
+            input::msg_qos(), 
+            input::usr(), 
+            input::pwd());
+            println!("----------------------------------------------------------------");
     }
 }
